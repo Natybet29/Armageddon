64b768bc593eec2962473233a7fbb5485da33fc6
Natybet29 <92266341+Natybet29@users.noreply.github.com>
2022-01-16 23:50:29 -0600

    hola
    Co-Authored-By: Viviana <92052488+Titaime@users.noreply.github.com>
    Co-Authored-By: CYBERLABS01 <86480574+CYBERLABS01@users.noreply.github.com>

--- Armageddon/Assets/Scripts/Armageddon/Scripts/Enemy.cs
+++ Armageddon/Assets/Scripts/Armageddon/Scripts/Enemy.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// This script defines 'Enemy's' health and behavior. 
-/// </summary>
 public class Enemy : MonoBehaviour {
 
     #region FIELDS
@@ -18,35 +15,31 @@ public class Enemy : MonoBehaviour {
     public GameObject destructionVFX;
     public GameObject hitEffect;
     
-    [HideInInspector] public int shotChance; //probability of 'Enemy's' shooting during tha path
-    [HideInInspector] public float shotTimeMin, shotTimeMax; //max and min time for shooting from the beginning of the path
-    #endregion
+    [HideInInspector] public int shotChance; 
+    [HideInInspector] public float shotTimeMin, shotTimeMax; 
 
     private void Start()
     {
         Invoke("ActivateShooting", Random.Range(shotTimeMin, shotTimeMax));
     }
 
-    //coroutine making a shot
+    
     void ActivateShooting() 
     {
-        if (Random.value < (float)shotChance / 100)                             //if random value less than shot probability, making a shot
+        if (Random.value < (float)shotChance / 100)                            
         {                         
             Instantiate(Projectile,  gameObject.transform.position, Quaternion.identity);             
         }
     }
 
-    //method of getting damage for the 'Enemy'
     public void GetDamage(int damage) 
     {
-        health -= damage;           //reducing health for damage value, if health is less than 0, starting destruction procedure
+        health -= damage;          
         if (health <= 0)
             Destruction();
         else
             Instantiate(hitEffect,transform.position,Quaternion.identity,transform);
     }    
-
-    //if 'Enemy' collides 'Player', 'Player' gets the damage equal to projectile's damage value
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
@@ -58,10 +51,9 @@ public class Enemy : MonoBehaviour {
         }
     }
 
-    //method of destroying the 'Enemy'
     void Destruction()                           
     {        
-        Instantiate(destructionVFX, transform.position, Quaternion.identity); 
+        Instantiate(destructionVFX,transform.position,Quaternion.identity); 
         Destroy(gameObject);
     }
-}
+ }
