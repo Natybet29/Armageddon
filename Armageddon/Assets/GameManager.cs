a4b45d354c2d2f05711a1ebff60522ba6af31343
Natybet29 <92266341+Natybet29@users.noreply.github.com>
2022-01-19 23:27:40 -0600

    hola

--- Armageddon/Assets/GameManager.cs
+++ Armageddon/Assets/GameManager.cs
@@ -2,36 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-public class GameManager : MonoBehaviour
+public class Armageddon : MonoBehaviour
 {
-    
-    public Scene lvl1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-public void Volverajugar() {
-    SceneManager.LoadScene("Armageddon");
-}
+    public void PLAY()
+    {       
+       SceneManager.LoadScene("NavesSpace");
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene("NavesSpace");
+    }
 
-    public void Empezarajugar() {
-    SceneManager.LoadScene("Armageddon");
-   }
+    public void  OUT()
+    {
+        Application.Quit();
+    }
 
-   public void Salirdeljuego() {
-       Application.Quit();
-   }
-
-   public void Iramenu() {
-       SceneManager.LoadScene("Menu principal");
-   }
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
 }
